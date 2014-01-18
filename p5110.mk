cdd8dda4369d57a1d506bd7d595d9d070a99a629
Android-Andi <skate4life@gmx.de>
2014-01-18 01:41:18 +0100

    P5110: Fix bringup DERP
    1. copy twrp.fstab into recovery
    
    Change-Id: Ief482ea32b3e79fdb53ac93160f5bf04c8f11e18

--- p5110.mk
+++ p5110.mk
@@ -25,6 +25,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/espresso10wifi \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
+# TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab
+
 # IR packages
 PRODUCT_PACKAGES += \
     consumerir.piranha
